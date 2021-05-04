d9b34ee53405fe0500e9f5b36a619a4f0c06da97
navarrs <ingrid.navarro.an@gmail.com>
2021-05-04 17:13:26 -0600

    started: velocity control version

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import os
 import magnum as mn
 import numpy as np
 import torch
+import safety_verify
 
 from controller.common.environments import SimpleRLEnv
 from controller.common.utils import resize, observations_to_image
@@ -16,6 +17,28 @@ from controller.controller import BaseController, ControllerType
 from habitat import logger
 from habitat.utils.visualizations.utils import images_to_video
 from habitat.sims.habitat_simulator.actions import HabitatSimActions
+
+BLACK_LIST = ["top_down_map", "fog_of_war_mask", "agent_map_coord"]
+
+
+def unroll_results(observations, results, action):
+    observations, rewards, dones, infos = [
+        list(x) for x in zip(*observations)
+    ]
+
+    if action == HabitatSimActions.STOP:
+        for k, v in infos[0].items():
+            if k in BLACK_LIST:
+                continue
+            if "collisions" in k:
+                if results.get("collision_count") == None:
+                    results["collision_count"] = []
+                results["collision_count"].append(v["count"])
+            else:
+                if results.get(k) == None:
+                    results[k] = []
+                results[k].append(v)
+    return observations, results, dones, infos
 
 
 def run_exp(exp_config: str) -> None:
@@ -27,8 +50,8 @@ def run_exp(exp_config: str) -> None:
         os.makedirs(config.VIDEO_DIR)
 
     with SimpleRLEnv(config=config) as env:
-        base = BaseController(config, 
-                              obs_space=env.observation_space, 
+        base = BaseController(config,
+                              obs_space=env.observation_space,
                               act_space=env.action_space,
                               sim=env.habitat_env.sim)
 
@@ -40,50 +63,76 @@ def run_exp(exp_config: str) -> None:
         # Fallback controller
         # @TODO: add fallback controller
         # fb_controller = base.build_controller(ControllerType.FALLBACK)
-
-        vc = habitat_sim.physics.VelocityControl()
-        if config.ROBOT.velocity_control:
-            vc.controlling_lin_vel = True
-            vc.lin_vel_is_local = True
-            vc.controlling_ang_vel = True
-            vc.ang_vel_is_local = True
-            vc.linear_velocity = np.array([0, 0, 1.0])
         
         # ----------------------------------------------------------------------
+        # Safety verification
+        verify = safety_verify.Verify(cell_size=.125)
+        verify.gen_primitive_lib(
+            np.array([.25]), np.linspace(-np.pi/12, np.pi/12, 3))
+
+        # ----------------------------------------------------------------------
+        # Define desired parameters
+        vc_desired = habitat_sim.physics.VelocityControl()
+        vc_desired.controlling_lin_vel = True
+        vc_desired.lin_vel_is_local = True
+        vc_desired.controlling_ang_vel = True
+        vc_desired.ang_vel_is_local = True
+
+        # @TODO: change these parameters ?
+        vc_desired.linear_velocity = np.array([0, 0, -1.0])
+        vc_desired.angular_velocity = np.array([0.0, 0.0, 0.0])
+
+        # note: this is the velocity we will feed to the environment
+        vc_current = vc_desired
+
+        # ----------------------------------------------------------------------
+        results = {}
         for i, episode in enumerate(env.episodes):
             if (i+1) > config.NUM_EPISODES:
                 break
-            
+
             frames = []
             observations = [env.reset()]
             bb_controller.reset()
             dones = None
+            infos = None
             goal_pos = env.current_episode.goals[0].position
-            
+            scene_id = env.current_episode.scene_id
+            episode_id = env.current_episode.episode_id
+
+            if "van-gogh" in scene_id:
+                continue
+
             while not env.habitat_env.episode_over:
 
-                # 1. Compute blackbox controller action
+                # Compute blackbox controller action
+                # --------------------------------------------------------------
                 action = bb_controller.get_next_action(
                     observations, deterministic=True, dones=dones, goal_pos=goal_pos)
-                
-                # @TODO: convert this to vel_control 
-                
-                # 2. @TODO: Compute future estimates
 
-                # 3. @TODO: Verify safety of reachable set
-                safe = True
-                if not safe:
-                    # 4. @TODO: Compute fallback controller action
-                    action = None
+                # @TODO: convert the action to vel_control
+                # --------------------------------------------------------------
+                vc_bb = habitat_sim.physics.VelocityControl()
 
-                # 5. Take a step
-                observations = [env.step(action)]
-                observations, rewards, dones, infos = [
-                    list(x) for x in zip(*observations)
-                ]
+                # Verify safety of reachable set
+                # --------------------------------------------------------------
+                safe = verify.verify_safety(infos, 6, action, verbose=False)
+                if not safe and config.CONTROLLERS.use_fallback:
+                    # @TODO: Compute fallback controller action
+                    # action = fb_controller.get_next_action(observations)
+                    vc_fb = fb_controller.get_next_action(observations)
 
+                # Take a step
+                observations = [env.step(vc_current)]
+                observations, results, dones, infos = unroll_results(
+                    observations, results, action)
                 frame = observations_to_image(observations[0], infos[0])
                 frames.append(frame)
+
+            if (i+1) % config.LOG_UPDATE == 0:
+                logger.info(f"Metrics for {i+1} episodes")
+                for k, v in results.items():
+                    logger.info(f"\t -- avg. {k}: {np.asarray(np.mean(v))}")
 
             # save episode
             if frames and len(config.VIDEO_OPTION) > 0:
@@ -95,6 +144,10 @@ def run_exp(exp_config: str) -> None:
                         episode.episode_id, i
                     )
                 )
+                
+        logger.info(f"Done. Metrics for {i+1} episodes")
+        for k, v in results.items():
+            logger.info(f"\t -- avg. {k}: {np.asarray(np.mean(v))}")
 
         env.close()
 
---
+++ safety_verify.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+
+def SO2Rotation(theta):
+    return(np.array([[np.cos(theta), -np.sin(theta)],
+                     [np.sin(theta), np.cos(theta)]]))
+
+
+def SE2Transformation(p, theta):
+    T = np.eye(3)
+    R = SO2Rotation(theta)
+    T[:2, :2] = R
+    T[:2, 2] = np.array(p).flatten()
+    return(T)
+
+
+class Verify:
+    def __init__(self, cell_size=.125):
+        self.cell_size = cell_size
+        self.primitive_lib = np.zeros((3, 3, 1))
+
+    def gen_primitive_lib(self, velocities, steers, dt=1):
+        velocities /= self.cell_size
+        controls = np.meshgrid(velocities, steers)
+        controls = np.vstack((controls[0].flatten(), controls[1].flatten())).T
+        lib = np.zeros((3, 3, controls.shape[0]))
+        for i in range(controls.shape[0]):
+            velocity, steer = controls[i, 0], controls[i, 1]
+            if steer != 0:
+                velocity = 0
+            lib[:, :, i] = SE2Transformation(
+                np.array([[1, 0]]).T*velocity*dt, steer)
+        self.primitive_lib = lib
+
+    def verify_safety(self, infos, T, action, threshold=.3, verbose=False):
+        if (self.primitive_lib == 0).all():
+            print("Error: Please initialize primitive library")
+            return(False)
+
+        if infos:
+            # print(infos[0]['top_down_map']['agent_map_coord'],infos[0]['top_down_map']['agent_angle'])
+
+            collision_map = infos[0]['top_down_map']['map']
+            branching_factor = self.primitive_lib.shape[2]
+            currentpose = SE2Transformation(
+                infos[0]['top_down_map']['agent_map_coord'], infos[0]['top_down_map']['agent_angle'])
+            # print(currentpose)
+            if action == 1:
+                startPose = np.dot(self.primitive_lib[:, :, 1], currentpose)
+            elif action == 2:
+                startPose = np.dot(self.primitive_lib[:, :, 0], currentpose)
+            elif action == 3:
+                startPose = np.dot(self.primitive_lib[:, :, 2], currentpose)
+            else:
+                startPose = currentpose
+            num_collisions = 0
+            open_set = startPose.reshape((3, 3, 1))
+            for t in range(T):
+                new_open_set = []
+                for i in range(open_set.shape[2]):
+                    for j in range(self.primitive_lib.shape[2]):
+                        new_pose = np.dot(
+                            open_set[:, :, i], self.primitive_lib[:, :, j])
+                        p = new_pose[:2, 2].astype(int).flatten()
+                        index = (p[0], p[1])
+                        
+                        # print(index)
+                        if index[0] < 0 or index[0] >= collision_map.shape[0] or index[1] < 0 or index[1] >= collision_map.shape[1] or collision_map[index] == 0:
+                            num_collisions += branching_factor**(T-t-1)
+                        else:
+                            new_open_set.append(new_pose)
+                if len(new_open_set) == 0:
+                    break
+                elif len(new_open_set) == 1:
+                    open_set = new_open_set[0].reshape((3, 3, 1))
+                else:
+                    open_set = np.dstack(new_open_set)
+
+            prob = num_collisions/(branching_factor**T)
+            if verbose:
+                print("Collision probability: ", prob)
+            return(prob < threshold)
+        else:
+            return(True)
